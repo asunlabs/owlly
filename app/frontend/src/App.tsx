3ca4bb6fd05deb7a0eb001061652c4740d785492
designerasun <asuntheawesome@gmail.com>
2022-11-12 23:28:53 +0900

    ENH:add wails frontend

---
+++ app/frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div id="app" className="App">
+      <header className="App-header">
+        <p>
+          Welcome to your new <code>wails/react</code> project.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
