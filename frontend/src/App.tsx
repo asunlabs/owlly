dbeba26d7909a60181b8920061235c2398b8d451
designerasun <asuntheawesome@gmail.com>
2022-11-13 23:07:15 +0900

    ENH:add basic app ui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,136 @@
-import {useState} from 'react';
-import logo from './assets/images/logo-universal.png';
-import './App.css';
-import {Greet} from "../wailsjs/go/main/App";
+import { FormEventHandler, useState } from "react";
+import banner from "./assets/images/banner.png";
+import "./App.css";
+import owllyConfig from "../../owlly.json";
 
+// TODO add dynamic input value for owlly config
 function App() {
-    const [resultText, setResultText] = useState("Please enter your name below 👇");
-    const [name, setName] = useState('');
-    const updateName = (e: any) => setName(e.target.value);
-    const updateResultText = (result: string) => setResultText(result);
+  // @dev footer modal local state
+  const [githubModal, setGithubModal] = useState("");
+  const [linkedinModal, setLinkedinModal] = useState("");
+  const [gmailModal, setGmailModal] = useState("");
 
-    function greet() {
-        Greet(name).then(updateResultText);
+  // @dev owlly config local state
+  const [triggerName, setTriggerName] = useState(owllyConfig.triggerName);
+
+  function handleTriggerName(e: React.ChangeEvent<HTMLInputElement>) {
+    e.target.value;
+    setTriggerName(e.target.value);
+  }
+
+  function useHover(_info: string) {
+    const info = _info.toLowerCase();
+
+    let modalText = {
+      github: "",
+      linkedin: "",
+      gmail: "",
+      default: "",
+    };
+
+    switch (info) {
+      case "github":
+        modalText.github = "https://github.com/asunlabs/owlly";
+        setGithubModal(modalText.github);
+        break;
+      case "linkedin":
+        modalText.linkedin = "https://www.linkedin.com/in/jakesung";
+        setLinkedinModal(modalText.linkedin);
+        break;
+      case "gmail":
+        modalText.gmail = "nellow1102@gmail.com";
+        setGmailModal(modalText.gmail);
+        break;
+      default:
+        break;
     }
+  }
+  return (
+    <div id="app">
+      <div id="owlly">
+        <img src={banner} alt="banner" id="frontBanner" />
+      </div>
+      <div id="body">
+        <div id="userConfig">
+          <h2>Let owlly take over your chores!</h2>
+          <form>
+            <fieldset>
+              <legend>Owlly</legend>
+              <input
+                type="text"
+                name="trigger"
+                id="trigger"
+                placeholder="trigger name here"
+                value={triggerName}
+                onChange={(e) => handleTriggerName(e)}
+              />
+            </fieldset>
 
-    return (
-        <div id="App">
-            <img src={logo} id="logo" alt="logo"/>
-            <div id="result" className="result">{resultText}</div>
-            <div id="input" className="input-box">
-                <input id="name" className="input" onChange={updateName} autoComplete="off" name="input" type="text"/>
-                <button className="btn" onClick={greet}>Greet</button>
-            </div>
+            <fieldset>
+              <legend>Slack</legend>
+              <input
+                type="text"
+                name="token"
+                id="token"
+                placeholder="oauth token here"
+                value={owllyConfig.slackBotOauthToken}
+              />
+              <input
+                type="text"
+                name="channelId"
+                id="channelId"
+                placeholder="channel id here"
+                value={owllyConfig.slackChannelID}
+              />
+              <input
+                type="text"
+                name="userId"
+                id="userId"
+                placeholder="user id here"
+                value={owllyConfig.slackUserID}
+              />
+              <input
+                type="text"
+                name="username"
+                id="username"
+                placeholder="username here"
+                value={owllyConfig.slackUserName}
+              />
+            </fieldset>
+            <input type="submit" id="sendDMButton" value="Send DM" />
+          </form>
         </div>
-    )
+        <div id="welcomePanel">
+          <h2>Welcome to owlly :)</h2>
+          <p>Let your teammate know .env changes instantly with Owlly.</p>
+        </div>
+      </div>
+
+      <ul id="footer">
+        <li
+          onMouseOver={() => useHover("github")}
+          onMouseLeave={() => setGithubModal("")}
+        >
+          Github
+          <span className="footerModal">{githubModal}</span>
+        </li>
+        <li
+          onMouseOver={() => useHover("linkedin")}
+          onMouseLeave={() => setLinkedinModal("")}
+        >
+          Linkedin
+          <span className="footerModal">{linkedinModal}</span>
+        </li>
+        <li
+          onMouseOver={() => useHover("gmail")}
+          onMouseLeave={() => setGmailModal("")}
+        >
+          Gmail
+          <span className="footerModal">{gmailModal}</span>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
-export default App
+export default App;
