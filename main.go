18120a6e63605d2337ce8359c9fcc28bf018f057
designerasun <asuntheawesome@gmail.com>
2022-11-14 23:55:29 +0900

    CLN:code cleanup

--- main.go
+++ main.go
@@ -24,12 +24,11 @@ func main() {
 			Assets: assets,
 		},
 		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
-		OnStartup:        app.startup,
+		OnStartup: app.startup,
 		Bind: []interface{}{
 			app,
 		},
 	})
-
 	if err != nil {
 		println("Error:", err.Error())
 	}
