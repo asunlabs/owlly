d2dd0087f64667f7c2ef75317074f60ce967d93d
developerasun <nellow1102@gmail.com>
2022-11-26 23:10:19 +0900

    ENH:set hash router for app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,19 @@
 import { SlackContextProvider } from './context/DefaultState';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import EnvNotifier from './pages/bot/EnvNotifier';
+import { Layout } from './pages/Layout';
 
 function App() {
   return (
     <div id="app">
-      <SlackContextProvider>
-        <EnvNotifier />
-      </SlackContextProvider>
+      <HashRouter>
+        <SlackContextProvider>
+          <Routes>
+            <Route path="/" element={<Layout />} />
+            <Route path="bot/env-notifier" element={<EnvNotifier />} />
+          </Routes>
+        </SlackContextProvider>
+      </HashRouter>
     </div>
   );
 }
