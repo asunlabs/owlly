75a86a194c9ab18aea7e439f62eeeba2b124074f
designerasun <asuntheawesome@gmail.com>
2022-11-26 19:37:20 +0900

    BLD:set path ailas for tsconfig and vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { ViteAliases } from 'vite-aliases';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react(), ViteAliases()],
+
+  // ! PATH: incorrect: "/src", correct: "./src"
+  resolve: {
+    alias: [{ find: '@', replacement: path.resolve(__dirname, './src') }],
+  },
 });
