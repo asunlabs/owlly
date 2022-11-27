23b166ec63678fb4a11d5376fd039088d95a6b3c
developerasun <nellow1102@gmail.com>
2022-11-27 19:08:21 +0900

    CLN:set global app setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,16 +2,26 @@ import { SlackContextProvider } from './context/DefaultState';
 import { HashRouter, Routes, Route } from 'react-router-dom';
 import EnvNotifier from './pages/bot/EnvNotifier';
 import { Layout } from './pages/Layout';
+import { SlackConfig } from './pages/account/SlackConfig';
+import { FaucetGetter } from './pages/automation/FaucetGetter';
+import { Login } from './pages/account/Login';
+import { App_ } from './_App';
+import './App.css';
 
 function App() {
   return (
     <div id="app">
       <HashRouter>
         <SlackContextProvider>
-          <Routes>
-            <Route path="/" element={<Layout />} />
-            <Route path="bot/env-notifier" element={<EnvNotifier />} />
-          </Routes>
+          <Layout>
+            <Routes>
+              <Route path="/" element={<App_ />} />
+              <Route path="account/login" element={<Login />} />
+              <Route path="account/slack" element={<SlackConfig />} />
+              <Route path="automation/faucet-getter" element={<FaucetGetter />} />
+              <Route path="bot/env-notifier" element={<EnvNotifier />} />
+            </Routes>
+          </Layout>
         </SlackContextProvider>
       </HashRouter>
     </div>
---
+++ frontend/src/_App.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+export interface IApp_Props {}
+
+export function App_(props: IApp_Props) {
+  return <div>root app screen</div>;
+}
