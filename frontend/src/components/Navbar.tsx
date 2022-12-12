e032b1d3420815751f9eaf7f5bd381a870c32bdb
developerasun <nellow1102@gmail.com>
2022-12-12 22:01:27 +0900

    ENH:add props to styled list comp

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -6,8 +6,6 @@ export const Navbar = styled.nav`
   display: flex;
   flex-flow: column nowrap;
   background-color: RGB(94 67 60);
-  height: 100vh;
-  width: 25%;
 `;
 
 export const Body = styled.div`
@@ -15,14 +13,20 @@ export const Body = styled.div`
   padding: 2rem;
 `;
 
-export const List = styled.li`
+// @dev define props for styled-component
+export const List = styled.li<{ toggle: boolean }>`
+  display: ${(props) => (props.toggle ? 'grid' : 'none')};
+  grid-template-columns: repeat(2, 1fr);
+  align-items: center;
   list-style: none;
-  padding: 1rem;
-  font-size: large;
+  padding: 2rem;
+  font-size: 1rem;
   cursor: pointer;
 
-  &:hover {
-    background-color: RGB(222 210 158);
+  // icon
+  svg {
+    font-size: 1.3rem;
+    padding: 1rem 0;
   }
 `;
 
@@ -31,11 +35,22 @@ export const Dropdown = styled.div`
   flex-flow: column nowrap;
 `;
 
-export const Anchor = styled.a`
-  text-decoration: none;
-`;
+export const Title = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  align-items: center;
+  list-style: none;
+  padding: 1.5rem;
+  font-size: 1.4rem;
+  cursor: pointer;
 
-export const SearchForm = styled.form`
-  display: flex;
-  flex-flow: column nowrap;
+  // icon
+  svg {
+    font-size: 2rem;
+    padding: 1rem;
+  }
+
+  &:hover {
+    background-color: RGB(222 210 158);
+  }
 `;
