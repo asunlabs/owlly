3709e4c4db35625cd143f0ef80841da663592845
developerasun <nellow1102@gmail.com>
2022-12-12 22:02:15 +0900

    ENH:add dropdown to sidebar ui

--- frontend/src/pages/Layout.tsx
+++ frontend/src/pages/Layout.tsx
@@ -1,39 +1,64 @@
 import { Background } from '@owlly/components/Background';
-import { Anchor, Body, Dropdown, List, Navbar } from '@owlly/components/Navbar';
+import { Body, Dropdown, List, Navbar, Title } from '@owlly/components/Navbar';
 import * as React from 'react';
 import { Link } from 'react-router-dom';
+import {
+  AiOutlineLogin,
+  AiOutlineHome,
+  AiOutlineMail,
+  AiOutlineMoneyCollect,
+  AiOutlineLock,
+  AiOutlineRobot,
+} from 'react-icons/ai';
+import { MdAccountCircle } from 'react-icons/md';
 
 export interface ILayoutProps {
   children?: React.ReactNode;
 }
 
 export function Layout({ children }: ILayoutProps) {
+  const [toggle, setToggle] = React.useState(false);
+
   return (
-    <>
-      <Background id="layout">
-        <Navbar>
-          <List>
-            <Link to={'/'}>Home</Link>
+    <Background id="layout">
+      <Navbar>
+        <List toggle>
+          <AiOutlineHome />
+          <Link to={'/'}>Home</Link>
+        </List>
+
+        <Dropdown onClick={() => setToggle(!toggle)}>
+          <Title>
+            <MdAccountCircle />
+            Account
+          </Title>
+          <List toggle={toggle}>
+            <AiOutlineLogin />
+            <Link to={'account/login'}>Sign in</Link>
+          </List>
+          <List toggle={toggle}>
+            <AiOutlineMail />
+            <Link to={'account/slack'}>Slack</Link>
+          </List>
+        </Dropdown>
+
+        <Dropdown onClick={() => setToggle(!toggle)}>
+          <Title>
+            <AiOutlineRobot />
+            Bot
+          </Title>
+          <List toggle={toggle}>
+            <AiOutlineMoneyCollect />
+            <Link to={'bot/faucet-getter'}>Faucet</Link>
           </List>
 
-          <List>
-            <Link to={'account/login'}>🗝️ Sign in</Link>
+          <List toggle={toggle}>
+            <AiOutlineLock />
+            <Link to={'bot/env-notifier'}>Dotenv</Link>
           </List>
-
-          <List>
-            <Link to={'account/slack'}>📩 Slack</Link>
-          </List>
-
-          <List>
-            <Link to={'automation/faucet-getter'}>💸 Faucet</Link>
-          </List>
-
-          <List>
-            <Link to={'bot/env-notifier'}>🔐 Dotenv</Link>
-          </List>
-        </Navbar>
-        <Body id="body">{children}</Body>
-      </Background>
-    </>
+        </Dropdown>
+      </Navbar>
+      <Body id="body">{children}</Body>
+    </Background>
   );
 }
