b1f4a2ea5c6f07c1bc8ced9e36e17cf849bd2ea6
developerasun <nellow1102@gmail.com>
2022-12-14 22:22:58 +0900

    ENH:set props for sidebar ui comps

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,32 +1,39 @@
-import { breakpoints } from '@owlly/context/DefaultState';
-import * as React from 'react';
+import { ReactNode } from 'react';
 import styled from 'styled-components';
 
 export const Navbar = styled.nav`
   display: flex;
   flex-flow: column nowrap;
   background-color: RGB(94 67 60);
+  width: fit-content;
 `;
 
 export const Body = styled.div`
-  width: 75%;
   padding: 2rem;
 `;
 
+export interface IListProps {
+  toggle?: boolean;
+}
+
 // @dev define props for styled-component
-export const List = styled.li<{ toggle: boolean }>`
+export const List = styled.li<IListProps>`
   display: ${(props) => (props.toggle ? 'grid' : 'none')};
-  grid-template-columns: repeat(2, 1fr);
+  grid-template-columns: 1fr 2fr;
   align-items: center;
   list-style: none;
-  padding: 2rem;
-  font-size: 1rem;
+  padding: 1rem;
   cursor: pointer;
+  border-radius: 20%;
 
   // icon
   svg {
     font-size: 1.3rem;
-    padding: 1rem 0;
+    padding-left: 3rem;
+  }
+
+  &:hover {
+    background-color: RGB(222 210 158);
   }
 `;
 
@@ -35,22 +42,25 @@ export const Dropdown = styled.div`
   flex-flow: column nowrap;
 `;
 
-export const Title = styled.div`
+export interface ITitleProps {
+  isHome?: boolean;
+}
+
+export const Title = styled.div<ITitleProps>`
   display: grid;
-  grid-template-columns: repeat(2, 1fr);
+  grid-template-columns: 1fr 2fr;
   align-items: center;
   list-style: none;
   padding: 1.5rem;
   font-size: 1.4rem;
   cursor: pointer;
+  background-color: ${({ isHome }) => (isHome ? 'RGB(222 210 158)' : 'none')};
+  color: ${({ isHome }) => (isHome ? 'black' : '')};
+  font-weight: ${({ isHome }) => (isHome ? 'bold' : 'none')};
 
   // icon
   svg {
     font-size: 2rem;
     padding: 1rem;
-  }
-
-  &:hover {
-    background-color: RGB(222 210 158);
   }
 `;
