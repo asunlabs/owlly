419db8fef3d22b690f274954e471d50f995ce9b1
developerasun <nellow1102@gmail.com>
2022-12-14 22:22:25 +0900

    ENH:set dropdown states

--- frontend/src/pages/Layout.tsx
+++ frontend/src/pages/Layout.tsx
@@ -10,51 +10,96 @@ import {
   AiOutlineLock,
   AiOutlineRobot,
 } from 'react-icons/ai';
-import { MdAccountCircle } from 'react-icons/md';
+import { MdAccountCircle, MdOutlineContactSupport } from 'react-icons/md';
+import { FaSlack } from 'react-icons/fa';
 
 export interface ILayoutProps {
   children?: React.ReactNode;
 }
 
 export function Layout({ children }: ILayoutProps) {
-  const [toggle, setToggle] = React.useState(false);
+  const [toggle, setToggle] = React.useState({
+    isToggle: false,
+    account: false,
+    bot: false,
+    help: false,
+  });
 
   return (
     <Background id="layout">
       <Navbar>
-        <List toggle>
+        <Title isHome={true}>
           <AiOutlineHome />
           <Link to={'/'}>Home</Link>
-        </List>
+        </Title>
 
-        <Dropdown onClick={() => setToggle(!toggle)}>
+        <Dropdown
+          id="dropdown-account"
+          onClick={() =>
+            setToggle({
+              isToggle: true,
+              account: true,
+              bot: false,
+              help: false,
+            })
+          }
+        >
           <Title>
             <MdAccountCircle />
             Account
           </Title>
-          <List toggle={toggle}>
+          <List toggle={toggle.isToggle && toggle.account ? true : false}>
             <AiOutlineLogin />
             <Link to={'account/login'}>Sign in</Link>
           </List>
-          <List toggle={toggle}>
-            <AiOutlineMail />
+          <List toggle={toggle.isToggle && toggle.account ? true : false}>
+            <FaSlack />
             <Link to={'account/slack'}>Slack</Link>
           </List>
         </Dropdown>
 
-        <Dropdown onClick={() => setToggle(!toggle)}>
+        <Dropdown
+          id="dropdown-bot"
+          onClick={() =>
+            setToggle({
+              isToggle: true,
+              account: false,
+              bot: true,
+              help: false,
+            })
+          }
+        >
           <Title>
             <AiOutlineRobot />
             Bot
           </Title>
-          <List toggle={toggle}>
+          <List toggle={toggle.isToggle && toggle.bot ? true : false}>
             <AiOutlineMoneyCollect />
             <Link to={'bot/faucet-getter'}>Faucet</Link>
           </List>
-
-          <List toggle={toggle}>
+          <List toggle={toggle.isToggle && toggle.bot ? true : false}>
             <AiOutlineLock />
             <Link to={'bot/env-notifier'}>Dotenv</Link>
+          </List>
+        </Dropdown>
+
+        <Dropdown
+          onClick={() =>
+            setToggle({
+              isToggle: true,
+              account: false,
+              bot: false,
+              help: true,
+            })
+          }
+        >
+          <Title>
+            <MdOutlineContactSupport />
+            Help
+          </Title>
+          <List toggle={toggle.isToggle && toggle.help ? true : false}>
+            <AiOutlineMail />
+            <Link to={'help/contact'}>Contact</Link>
           </List>
         </Dropdown>
       </Navbar>
