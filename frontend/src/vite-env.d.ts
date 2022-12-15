a23a8c0fe129233bc58cbe4d43d637e7aa3f81d3
developerasun <nellow1102@gmail.com>
2022-12-15 22:53:20 +0900

    TYP:add type declaration for vite envs

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_ALCHEMY_GOERLI_API_KEY: string;
+  readonly VITE_GOERLI_PRIVATE_KEY: string;
+  readonly VITE_GOERLI_SIGNER: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
