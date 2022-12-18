3557a736bce276d651a89e3df4967c0b733eda8f
developerasun <nellow1102@gmail.com>
2022-12-18 20:15:33 +0900

    CLN:split envbot create/update db op with gorm

--- config/db.go
+++ config/db.go
@@ -1,13 +1,18 @@
 package config
 
 import (
+	"github.com/fatih/color"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
-// ==================================================================== //  
-// ====================== Event-based CRUD DB OP ====================== //  
-// ==================================================================== // 
+// ==================================================================== //
+// ========================= Init SQLite3 DB ========================== //
+// ==================================================================== //
+
+var (
+	DB_HANDLE *gorm.DB
+)
 
 func __ConnectDB() (bool, string) {
 	_db, oErr := gorm.Open(sqlite.Open("owlly.db"), &gorm.Config{})
@@ -20,4 +25,29 @@ func __ConnectDB() (bool, string) {
 	DB_HANDLE = _db
 
 	return true, ""
+}
+
+// ==================================================================== //
+// ====================== Event-based CRUD DB OP ====================== //
+// ==================================================================== //
+// EnvBotDB OP
+
+func CreateEnvBotConfig(botConfig ModelEnvBot) {
+	cResult := DB_HANDLE.Create(botConfig)
+
+	if cResult.Error != nil {
+		color.Red("db.go: CreateEnvBotConfig failed to execute")
+	} else {
+		color.Green(("db.go:DONE: envbot config created"))
+	}
+}
+
+func UpdateEnvBotConfig(botConfig ModelEnvBot) {
+	uResult := DB_HANDLE.Model(ModelEnvBot{}).Where("id = ?", botConfig.ID).Updates(botConfig)
+
+	if uResult.Error != nil {
+		color.Red("db.go: UpdateEnvBotConfig failed to execute")
+	} else {
+		color.Green(("db.go:DONE: envbot config updated"))
+	}
 }
