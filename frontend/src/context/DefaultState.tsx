351a7d8289a1972885a54d013a6e92ba1e768b5b
developerasun <nellow1102@gmail.com>
2022-12-18 15:28:38 +0900

    ENH:add context comments, default siwe message, network id

--- frontend/src/context/DefaultState.tsx
+++ frontend/src/context/DefaultState.tsx
@@ -1,5 +1,13 @@
 import * as React from 'react';
+import { ISlackContextProvider, ISiweProps } from './types.d';
 
+/*
+Context list
+1. slack config
+2. wallet key pair
+3. owlly login info
+4. dropdown state
+*/
 export const SlackContext = React.createContext({
   slackContext: {
     triggerName: '',
@@ -20,10 +28,6 @@ export const SlackContext = React.createContext({
     }>
   ) => {},
 });
-
-export interface ISlackContextProvider {
-  children?: React.ReactNode;
-}
 
 export function SlackContextProvider({ children }: ISlackContextProvider) {
   const [slackContext, setSlackContext] = React.useState({
@@ -63,4 +67,17 @@ export const breakpoints = {
 export const EVENT_SLACK = {
   update: 'SLACK_UPDATE_EVENT',
   delete: 'SLACK_DELETE_EVENT',
+};
+
+export const NETWORK_ID = {
+  goerli: 5,
+};
+
+export const SIWE_MESSAGE_PROPS: ISiweProps = {
+  domain: 'Owlly ver 0.3.2' || window.location.host,
+  address: '0xEcAB21327B6EbA1FB0631Dc9bBc5863B6B2be3E4',
+  statement: 'Owlly: Sign in with Ethereum',
+  uri: 'developerasun' || window.location.origin,
+  version: '1',
+  chainId: NETWORK_ID.goerli,
 };
