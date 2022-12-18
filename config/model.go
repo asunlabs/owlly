85424b2a5f615d04833a8b9152b746d85ea56175
developerasun <nellow1102@gmail.com>
2022-12-18 21:27:10 +0900

    ENH:improve gorm models

--- config/model.go
+++ config/model.go
@@ -7,28 +7,26 @@ import (
 // ==================================================================== //
 // =========================== Table models =========================== //
 // ==================================================================== //
-// @dev convention: Model prefix, gorm struct
+// @dev convention: Model prefix, gorm convention: ID, CreatedAt, UpdatedAt, DeletedAt
 
 type ModelEmailUser struct {
 	gorm.Model
-	Email    string `json:"email"`
-	Password string `json:"password"`
-	Username string `json:"username"`
+	Email    string `gorm:"unique;not null" json:"email"`
+	Password string `gorm:"not null" json:"password"`
+	Username string `gorm:"not null" json:"username"`
 }
 
 type ModelWalletUser struct {
 	gorm.Model
-	PrivateKey string `json:"privateKey"`
-	AlchemyKey string `json:"alchemyKey"`
-	Salt       string `json:"salt"`
+	PrivateKey string `gorm:"unique;not null" json:"privateKey"`
+	AlchemyKey string `gorm:"unique;not null" json:"alchemyKey"`
 }
 
 type ModelEnvBot struct {
-	gorm.Model                // embed gorm convention: ID, CreatedAt, UpdatedAt, DeletedAt
-	TriggerName        string `json:"triggerName"`
-	SlackBotOauthToken string `json:"slackBotOauthToken"`
-	SlackChannelID     string `json:"slackChannelID"`
-	SlackUserID        string `json:"slackUserID"`
-	SlackUserName      string `json:"slackUserName"`
-	Salt               string `json:"salt"`
+	gorm.Model                
+	TriggerName        string `gorm:"not null" json:"triggerName"`
+	SlackBotOauthToken string `gorm:"unique;not null" json:"slackBotOauthToken"`
+	SlackChannelID     string `gorm:"not null" json:"slackChannelID"`
+	SlackUserID        string `gorm:"not null" json:"slackUserID"`
+	SlackUserName      string `gorm:"not null" json:"slackUserName"`
 }
