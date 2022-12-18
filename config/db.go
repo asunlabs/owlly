b4b7b7aef36c3cc40f936035420ea52358ea1011
developerasun <nellow1102@gmail.com>
2022-12-18 21:27:29 +0900

    ENH:add email user DB CRUD

--- config/db.go
+++ config/db.go
@@ -1,6 +1,8 @@
 package config
 
 import (
+	"fmt"
+
 	"github.com/fatih/color"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
@@ -14,14 +16,14 @@ var (
 	DB_HANDLE *gorm.DB
 )
 
-func __ConnectDB() (bool, string) {
+func ConnectDB() (bool, string) {
 	_db, oErr := gorm.Open(sqlite.Open("owlly.db"), &gorm.Config{})
 
 	if oErr != nil {
 		return false, oErr.Error()
 	}
 
-	_db.AutoMigrate(&OwllyConfig{}, &User{})
+	_db.AutoMigrate(&ModelEmailUser{}, &ModelWalletUser{}, &ModelEnvBot{})
 	DB_HANDLE = _db
 
 	return true, ""
@@ -30,7 +32,7 @@ func __ConnectDB() (bool, string) {
 // ==================================================================== //
 // ====================== Event-based CRUD DB OP ====================== //
 // ==================================================================== //
-// EnvBotDB OP
+// EnvBot DB OP
 
 func CreateEnvBotConfig(botConfig ModelEnvBot) {
 	cResult := DB_HANDLE.Create(botConfig)
@@ -43,11 +45,59 @@ func CreateEnvBotConfig(botConfig ModelEnvBot) {
 }
 
 func UpdateEnvBotConfig(botConfig ModelEnvBot) {
-	uResult := DB_HANDLE.Model(ModelEnvBot{}).Where("id = ?", botConfig.ID).Updates(botConfig)
+	uResult := DB_HANDLE.Model(&botConfig).Where("id = ?", botConfig.ID).Updates(botConfig)
 
 	if uResult.Error != nil {
 		color.Red("db.go: UpdateEnvBotConfig failed to execute")
 	} else {
 		color.Green(("db.go:DONE: envbot config updated"))
+	}
+}
+
+// Account DB OP
+// sign up
+func CreateEmailUser(emailUser ModelEmailUser)  {
+	cResult := DB_HANDLE.Create(&emailUser)
+
+	if cResult.Error != nil {
+		color.Red("db.go: CreateEmailUser failed to execute")
+	} else {
+		color.Green(("db.go:DONE: new email user created"))
+	}
+}
+
+func ReadEmailUser(email string)  {
+	// db.First, db.Last only works with struct pointer parameter
+	rResult := DB_HANDLE.Where("email = ?", email).First(&ModelEmailUser{})
+
+	if rResult.Error != nil {
+		color.Red("db.go: UpdateEmailUser failed to execute")
+	} else {
+		color.Green(("db.go:DONE: new email user created"))
+	}
+}
+
+// update password
+// TODO add password hashing mechanism with https://github.com/golang/crypto/tree/master/bcrypt
+func UpdateEmailUser(id uint, newPassword string)  {
+	rResult := DB_HANDLE.Where("id = ?", id).First(&ModelEmailUser{})
+	
+	if rResult.Error != nil {
+		color.Red("db.go: UpdateEmailUser failed to execute")
+	} else {
+		// only update a record with the specified id
+		DB_HANDLE.Model(&ModelEmailUser{}).Where("id = ?", id).Update("Password", newPassword)
+		message := fmt.Sprintf("db.go:DONE: email user with id %v is updated", id)
+		color.Green(message)
+	}
+}
+
+func DeleteEmailUser(id uint)  {
+	rResult := DB_HANDLE.Where("id = ?", id).First(&ModelEmailUser{})
+
+	if rResult.Error != nil {
+		color.Red("db.go: DeleteEmailUser failed to execute")
+	} else {
+		DB_HANDLE.Where("id = ?", id).Delete(&ModelEmailUser{})
 	}
 }
