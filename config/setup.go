0039e699c1ee6d4263d15d171191ccf247c1f115
developerasun <nellow1102@gmail.com>
2022-12-18 21:28:07 +0900

    CLN:split db model and op from config

--- config/setup.go
+++ config/setup.go
@@ -4,12 +4,10 @@ import (
 	"fmt"
 
 	"github.com/fatih/color"
-	"gorm.io/driver/sqlite"
-	"gorm.io/gorm"
 )
 
 var (
-	Owlly       *OwllyConfig
+	EnvBot       *ModelEnvBot
 	SLACK_EVENT = map[string]string{
 		"update": "SLACK_UPDATE_EVENT",
 		"delete": "SLACK_DELETE_EVENT",
@@ -21,22 +19,6 @@ var (
 	}
 )
 
-type OwllyConfig struct {
-	gorm.Model                // embed gorm convention: ID, CreatedAt, UpdatedAt, DeletedAt
-	TriggerName        string `json:"triggerName"`
-	SlackBotOauthToken string `json:"slackBotOauthToken"`
-	SlackChannelID     string `json:"slackChannelID"`
-	SlackUserID        string `json:"slackUserID"`
-	SlackUserName      string `json:"slackUserName"`
-}
-
-type User struct {
-	gorm.Model
-	Email    string `json:"email"`
-	Password string `json:"password"`
-}
-
-// TODO split DB op
 // @dev get event values from front end and update config
 func New(
 	triggerName string,
@@ -45,15 +27,15 @@ func New(
 	slackUserID string,
 	slackUserName string,
 ) {
-	var _Owlly OwllyConfig
+	var _EnvBot ModelEnvBot
 
-	_Owlly.TriggerName = triggerName
-	_Owlly.SlackBotOauthToken = slackBotOauthToken
-	_Owlly.SlackChannelID = slackChannelID
-	_Owlly.SlackUserID = slackUserID
-	_Owlly.SlackUserName = slackUserName
+	_EnvBot.TriggerName = triggerName
+	_EnvBot.SlackBotOauthToken = slackBotOauthToken
+	_EnvBot.SlackChannelID = slackChannelID
+	_EnvBot.SlackUserID = slackUserID
+	_EnvBot.SlackUserName = slackUserName
 
-	Owlly = &_Owlly
+	EnvBot = &_EnvBot
 
 	if ok, _ := ConnectDB(); ok {
 		color.Green("Setup.go: DB connected")
@@ -62,32 +44,18 @@ func New(
 	}
 
 	// if record exists, update it
-	if exists := DB_HANDLE.First(Owlly); exists.Error == nil {
-		DB_HANDLE.Model(Owlly).Where("id = ?", Owlly.ID).Updates(Owlly)
+	if exists := DB_HANDLE.First(EnvBot); exists.Error == nil {
+		DB_HANDLE.Model(EnvBot).Where("id = ?", EnvBot.ID).Updates(EnvBot)
 	} else {
 		// if new, create it
-		cResult := DB_HANDLE.Create(Owlly)
+		cResult := DB_HANDLE.Create(EnvBot)
 
 		if cResult.Error != nil {
 			color.Red("Setup.go: DB create op failed")
 		}
-		msg := fmt.Sprintf("Setup.go: New config from GUI: %v saved to DB", Owlly)
+		msg := fmt.Sprintf("Setup.go: New config from GUI: %v saved to DB", EnvBot)
 		color.Cyan(msg)
 	}
 
 	color.Cyan("Setup.go: Envbot properly configured")
-}
-
-// TODO split DB op
-func ConnectDB() (bool, string) {
-	_db, oErr := gorm.Open(sqlite.Open("owlly.db"), &gorm.Config{})
-
-	if oErr != nil {
-		return false, oErr.Error()
-	}
-
-	_db.AutoMigrate(&OwllyConfig{}, &User{})
-	DB_HANDLE = _db
-
-	return true, ""
 }
