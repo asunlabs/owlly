c4ac72ff656dd7cd34e9cb55866e7b8b6f2a44e6
developerasun <nellow1102@gmail.com>
2022-12-21 23:29:46 +0900

    ENH:add email signup controller/listner

--- core/account/account.controller.go
+++ core/account/account.controller.go
@@ -1,3 +1,16 @@
 package account
 
+import (
+	"context"
+
+	"github.com/asunlabs/owlly/config"
+	"github.com/wailsapp/wails/v2/pkg/runtime"
+)
+
 // TODO add sign in, sign up event
+func ListenEmailSignUp(ctx context.Context)  {
+	runtime.EventsOn(ctx, config.AUTH_EVENT["sign-up"], func(optionalData ...interface{}) {
+		var newEmailUser config.ModelEmailUser
+		CreateEmailUser(newEmailUser)
+	})
+}
