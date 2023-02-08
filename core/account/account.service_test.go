642595c30c202d1f6915984fd94854821ad216be
developerasun <nellow1102@gmail.com>
2023-02-08 20:05:32 +0900

    TST:add a base test case boilerplate

---
+++ core/account/account.service_test.go
@@ -0,0 +1,21 @@
+package account
+
+import (
+	"testing"
+
+	"github.com/asunlabs/owlly/config"
+)
+
+// TODO add db mocking
+func TestEmailUser(t *testing.T) {
+	// set hardcoded value
+	newUser := config.ModelEmailUser{
+		Username: "test",
+		Email: "test@gmail.com",
+		Password: "123",
+	}
+
+
+	// call target method
+	CreateEmailUser(newUser)
+}
