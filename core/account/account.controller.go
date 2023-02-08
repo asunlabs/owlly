cbc80b570aac581785e694edb10d9c218b97c6ec
developerasun <nellow1102@gmail.com>
2023-02-08 20:05:15 +0900

    ENH:add wallet sign up api

--- core/account/account.controller.go
+++ core/account/account.controller.go
@@ -8,11 +8,22 @@ import (
 
 // @dev optional data returns as a string array
 func HandleEmailSignUp(ctx context.Context) {
-	runtime.EventsOn(ctx, config.AUTH_EVENT["sign-up"], func(optionalData ...interface{}) {
+	runtime.EventsOn(ctx, config.AUTH_EMAIL["sign-up"], func(optionalData ...interface{}) {
 		var newEmailUser config.ModelEmailUser
 		newEmailUser.Email = optionalData[0].(string)
 		newEmailUser.Password = optionalData[1].(string)
+		// newEmailUser.Username = optionalData[2].(string)
 
 		CreateEmailUser(newEmailUser)
+	})
+}
+
+func HandleWalletSignUp(ctx context.Context) {
+	runtime.EventsOn(ctx, config.AUTH_WALLET["sign-up"], func(optionalData ...interface{}) {
+		var newWalletUser config.ModelWalletUser
+		newWalletUser.PrivateKey = optionalData[0].(string)
+		newWalletUser.AlchemyKey = optionalData[1].(string)
+
+		CreateWalletUser(newWalletUser)
 	})
 }
