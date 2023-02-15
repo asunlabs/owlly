3dff391e38a23b7600cfed26144a1d754825da90
developerasun <nellow1102@gmail.com>
2023-02-15 19:44:53 +0900

    CLN:split SIWE components, add toast

--- frontend/src/pages/account/Login.tsx
+++ frontend/src/pages/account/Login.tsx
@@ -1,119 +1,25 @@
-import { Button } from '@owlly/components/Button';
+import { Button, GetToastByStatus } from '@owlly/components/Button';
 import * as React from 'react';
-import { ethers } from 'ethers';
-import { SiweMessage } from 'siwe';
 import { Form, FormTitle, Input, Label } from '@owlly/components/Form';
 import { MdOutlinePassword } from 'react-icons/md';
 import { AiOutlineMail } from 'react-icons/ai';
-import { EVENT_AUTH, NETWORK_ID } from '@owlly/context/DefaultState';
-import { ISignerInfoProps } from '@owlly/context/types';
+import { EVENT_AUTH } from '@owlly/context/DefaultState';
+import { ISignerInfoProps, TypeSignUp } from '@owlly/context/types';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import { BsKey } from 'react-icons/bs';
 import 'react-tabs/style/react-tabs.css';
-import { WrapperTab, WrapperTabPanel } from './../../components/Wrapper';
+import { WrapperTab, WrapperTabPanel } from '@owlly/components/Wrapper';
 import { Modal, ModalIconWrapper } from '@owlly/components/Modal';
 import { AiFillCloseCircle } from 'react-icons/ai';
 import { EventsEmit as SendWailsRequest } from '@wailsjs/runtime/runtime';
-
-/**
- * @dev domain: SIWE's domain. e.g. window.location.host => 'localhost:5173'
- * @dev origin: SIWE's URI. e.g.window.location.origin =>'http://localhost:5173'
- */
-function getSiweSetup() {
-  const domain = 'Owlly ver 0.3.1';
-  const origin = 'developerasun laptop';
-
-  const _window = window as any;
-  const ethereum = _window.ethereum;
-
-  // Web3 provider(metamask) already has a EOA signer in it, requiring Chrome extension to be installed
-  const provider = new ethers.providers.Web3Provider(ethereum);
-  const signer = provider.getSigner();
-
-  return {
-    domain,
-    origin,
-    ethereum,
-    provider,
-    signer,
-  };
-}
-
-/**
- *
- * @param address a signer's Ethereum address
- * @param statement a message that user will see when signing with SIWE
- * @dev version: current message version
- * @returns message returns a string message in a below format
- * 
-    "Owlly ver 0.3.1 wants you to sign in with your Ethereum account:
-    \n0xEcAB21327B6EbA1FB0631Dc9bBc5863B6B2be3E4\n
-    \nOwlly: Sign in with Ethereum\n
-    \nURI: developerasun laptop
-    \nVersion: 1
-    \nChain ID: 5
-    \nNonce: D40vyrk2K1M6DrHSX
-    \nIssued At: 2022-12-18T08:43:49.191Z"
- */
-function createSiweMessage(address: string, statement: string) {
-  const { domain, origin } = getSiweSetup();
-  const _message = new SiweMessage({
-    domain,
-    address,
-    statement,
-    uri: origin,
-    version: '1',
-    chainId: NETWORK_ID.goerli,
-  });
-
-  // @dev Returns a message ready to be signed according with the type defined in the object.
-  const message = _message.prepareMessage();
-  return { message };
-}
-
-async function connectWallet(callback: React.Dispatch<React.SetStateAction<ISignerInfoProps>>) {
-  try {
-    const { provider, signer } = getSiweSetup();
-    const accountArray = await provider.send('eth_requestAccounts', []);
-
-    if (accountArray[0] === (await signer.getAddress()).toLowerCase()) {
-      callback({
-        address: await signer.getAddress(),
-        balance: (await signer.getBalance()).toString(),
-        network: provider.network.name,
-        isActive: true,
-        isLogin: false,
-      });
-      return true;
-    }
-  } catch (err) {
-    console.log(err);
-  }
-}
-
-async function signInWithEthereum(callback: React.Dispatch<React.SetStateAction<ISignerInfoProps>>) {
-  // get user consent for metamask connection
-  const { signer } = getSiweSetup();
-  const ok = await connectWallet(callback);
-
-  if (ok) {
-    const { message } = createSiweMessage(await signer.getAddress(), 'Owlly: Sign in with Ethereum');
-
-    // get user consent for login
-    await signer.signMessage(message);
-
-    // make signer login status true
-    callback((prev) => ({
-      ...prev,
-      isLogin: true,
-    }));
-  }
-}
+import { HandleEmailSignUp } from '@wailsjs/go/main/Owlly';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function EmailLogin() {
   const [isModal, setIsModal] = React.useState(false);
 
-  function handleEmailSignUp(e: any) {
+  async function handleEmailSignUp(e: any) {
     e.preventDefault();
 
     const formData = new FormData(e.target);
@@ -121,6 +27,21 @@ function EmailLogin() {
     const password = formData.get('signup-password');
 
     SendWailsRequest(EVENT_AUTH.signUp, email, password);
+    const _response = await HandleEmailSignUp();
+
+    if (_response.Code == '200') {
+      GetToastByStatus('success', 'Email sign up success');
+
+      setTimeout(() => {
+        setIsModal(false);
+      }, 1800);
+    } else {
+      GetToastByStatus('failure', 'Email sign up failure');
+
+      setTimeout(() => {
+        setIsModal(false);
+      }, 1800);
+    }
   }
 
   return (
@@ -143,7 +64,6 @@ function EmailLogin() {
           Sign in
         </Button>
       </Form>
-
       {isModal && (
         <>
           <Modal modalType="email">
@@ -163,6 +83,7 @@ function EmailLogin() {
               <Button transparent={true} type={'submit'} id={'sign-up'}>
                 Sign up
               </Button>
+              <ToastContainer />
             </Form>
           </Modal>
         </>
@@ -170,8 +91,6 @@ function EmailLogin() {
     </>
   );
 }
-
-export type TypeSignUp = 'email' | 'wallet';
 
 function handleSignUpModal(signUpType: TypeSignUp, callback: React.Dispatch<React.SetStateAction<boolean>>) {
   switch (signUpType) {
@@ -260,24 +179,20 @@ function WalletLogin() {
 
 export function Login() {
   return (
-    <>
-      <WrapperTab>
-        <Tabs id="login-tab">
-          <TabList>
-            <Tab>Quick start</Tab>
-            <Tab>Experimental</Tab>
-          </TabList>
+    <WrapperTab id="login">
+      <Tabs id="login-tab">
+        <TabList>
+          <Tab>Quick start</Tab>
+          <Tab>Experimental</Tab>
+        </TabList>
 
-          <WrapperTabPanel>
-            <TabPanel className={'tab-panel'}>
-              <EmailLogin />
-            </TabPanel>
-            <TabPanel className={'tab-panel'}>
-              <WalletLogin />
-            </TabPanel>
-          </WrapperTabPanel>
-        </Tabs>
-      </WrapperTab>
-    </>
+        <TabPanel className={'tab-panel'}>
+          <EmailLogin />
+        </TabPanel>
+        <TabPanel className={'tab-panel'}>
+          <WalletLogin />
+        </TabPanel>
+      </Tabs>
+    </WrapperTab>
   );
 }
