42bc8bfc1654a6494ab8cb548678bb6d6e018594
developerasun <nellow1102@gmail.com>
2023-02-17 22:21:56 +0900

    TST: add test case for bcrypt password validation

--- core/account/account.service_test.go
+++ core/account/account.service_test.go
@@ -2,18 +2,56 @@ package account
 
 import (
 	"testing"
-	// "github.com/asunlabs/owlly/config"
+
+	"github.com/stretchr/testify/assert"
+	"golang.org/x/crypto/bcrypt"
 )
 
-// TODO add db mocking
-func TestEmailUser(t *testing.T) {
-	// set hardcoded value
-	// newUser := config.ModelEmailUser{
-	// 	Username: "test",
-	// 	Email:    "test@gmail.com",
-	// 	Password: "123",
-	// }
+type MockUser struct { 
+	Email string
+	Password string
+	Username string 
+	HashedPassword string
+}
 
-	// call target method
-	CreateEmailUser()
+func TestPasswordValidation(t *testing.T) {
+	user := MockUser {
+		Email: "user@gmail.com",
+		Password: "test",
+		Username: "user",
+		HashedPassword: "",
+	}
+
+	hash, gErr := bcrypt.GenerateFromPassword([]byte(user.Password), 0)
+
+	if gErr != nil {
+		t.Fail()
+		t.Log("bcrypt hash failure")
+	}
+
+	user.HashedPassword = string(hash)
+	t.Log("hashed password: ", user.HashedPassword)
+
+	passwordFromDB := []byte(user.HashedPassword)
+	correctPassword := []byte("test")
+
+	cErr := bcrypt.CompareHashAndPassword(passwordFromDB, correctPassword)
+
+	if cErr != nil {
+		t.Fail()
+		t.Log("Invalid password")
+	}
+
+	invalidPassword := []byte("test1")
+
+	cErrForInvalidPassword := bcrypt.CompareHashAndPassword(passwordFromDB, invalidPassword)
+	
+	if cErrForInvalidPassword != nil {
+		t.Log("Invalid password checked by bcrypt")
+
+		expected := "crypto/bcrypt: hashedPassword is not the hash of the given password"
+		actual := cErrForInvalidPassword.Error()
+
+		assert.Equal(t, expected, actual)
+	}
 }
