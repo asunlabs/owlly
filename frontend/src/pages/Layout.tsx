7c9ca6d8365ee0aec37de65cd9bfbec56b9501c9
developerasun <nellow1102@gmail.com>
2023-02-26 19:23:24 +0900

    BUG: fix outside click nav toggle

--- frontend/src/pages/Layout.tsx
+++ frontend/src/pages/Layout.tsx
@@ -1,9 +1,9 @@
 import { Background, LoadingView } from '@owlly/components/Background';
 import { Button, GetToastByStatus } from '@owlly/components/Button';
-import { Body, Dropdown, List, Navbar, Title } from '@owlly/components/Navbar';
+import { Dropdown, List, Navbar, Title } from '@owlly/components/Navbar';
 import * as React from 'react';
 import { Link } from 'react-router-dom';
-import { IDropdownProps, ILayoutProps } from '@owlly/context/types.d';
+import { IDropdownProps, ILayoutProps, IIsLaunchProps } from '@owlly/context/types.d';
 import {
   AiOutlineLogin,
   AiOutlineHome,
@@ -16,9 +16,9 @@ import {
 } from 'react-icons/ai';
 import { MdAccountCircle, MdOutlineContactSupport } from 'react-icons/md';
 import { FaSlack } from 'react-icons/fa';
-import { ToastContainer } from 'react-toastify';
 import loading from '@owlly/assets/images/loading.gif';
-import { ModalIconWrapper } from '@owlly/components/Modal';
+import { WrapperDivForCenter, WrapperModalIcon } from '@owlly/components/Wrapper';
+import { breakpoints } from '../context/constants';
 
 export function Layout({ children }: ILayoutProps) {
   const [toggle, setToggle] = React.useState(false);
@@ -27,13 +27,22 @@ export function Layout({ children }: ILayoutProps) {
     bot: false,
     help: false,
   });
-  const [isLaunch, setIsLaunch] = React.useState(false);
-  const ref = React.useRef<HTMLLIElement>(null);
+  const [isLaunch, setIsLaunch] = React.useState<IIsLaunchProps>({
+    disable: false,
+    launch: false,
+  });
 
   function handleAppLaunch() {
+    setIsLaunch((prev) => ({
+      ...prev,
+      disable: true,
+    }));
     GetToastByStatus('success', 'Launching Owlly...');
     setTimeout(() => {
-      setIsLaunch(true);
+      setIsLaunch((prev) => ({
+        ...prev,
+        launch: true,
+      }));
     }, 1800);
   }
 
@@ -58,103 +67,107 @@ export function Layout({ children }: ILayoutProps) {
     });
   }
 
-  function handleOutsideDropdown(e: any) {
-    if (ref.current && !ref.current.contains(e.target)) {
-      // prettier-ignore
-      setDropdown({ 
-        account: false, 
-        bot: false, 
-        help: false 
-      });
+  function handleResize() {
+    if (window.innerWidth >= parseInt(breakpoints.device.tablet)) {
+      setToggle(true);
     }
   }
 
-  // reset dropdown when user click outside navbar
-  React.useEffect(() => {
-    document.addEventListener('click', handleOutsideDropdown);
+  function handleOutsideDropdown(e: any) {
+    if (window.innerWidth < parseInt(breakpoints.device.tablet)) {
+      setToggle(false);
+    }
+  }
 
-    // cleanup event listener
+  // @dev React replaces `addEventListener` with SyntheticEvent for a few reasons: https://linguinecode.com/post/react-onclick-event-vs-js-addeventlistener
+  // SyntheticEvent is preferred over `addEventListener` generally
+  React.useEffect(() => {
+    window.addEventListener('resize', handleResize);
+
+    // have to clean up oneself
     return () => {
-      document.removeEventListener('click', handleOutsideDropdown);
+      window.removeEventListener('resize', handleResize);
     };
-  }, [ref]);
+  });
 
   return (
     <>
       <Background id="layout">
-        {/* app booting animation */}
-        {!isLaunch && (
-          <LoadingView>
-            <div id="description">
-              <img src={loading} alt="greeting banner" />
-              <h1>Welcome to Owlly</h1>
-              <Button id="launch-button" isDynamic={true} onClick={handleAppLaunch}>
-                Launch App
-              </Button>
-            </div>
-          </LoadingView>
-        )}
-
         {/* app contents */}
-        <Navbar ref={ref}>
+        <Navbar id="navbar">
+          {/* app booting animation */}
+          {!isLaunch.launch && (
+            <LoadingView>
+              <div id="description">
+                <img src={loading} alt="greeting banner" />
+                <h1>Welcome to Owlly</h1>
+                <Button id="launch-button" isDynamic={true} onClick={handleAppLaunch} disabled={isLaunch.disable}>
+                  Launch App
+                </Button>
+              </div>
+            </LoadingView>
+          )}
           <Title isHome={true}>
             <AiOutlineHome />
             <Link to={'/'}>Owlly</Link>
-            {/* TODO fix media query */}
-            <ModalIconWrapper onClick={handleToggle}>
-              {toggle ? <AiFillCloseCircle /> : <AiOutlineMenu />}
-            </ModalIconWrapper>
+            <WrapperModalIcon onClick={handleToggle}>
+              {toggle ? <AiFillCloseCircle className="trigger" /> : <AiOutlineMenu className="trigger" />}
+            </WrapperModalIcon>
           </Title>
 
-          {toggle && (
-            <div id="dropdowns">
-              <Dropdown id="account" onClick={handleDropdown}>
-                <Title>
-                  <MdAccountCircle />
-                  Account
-                </Title>
-                <List toggle={dropdown.account ? true : false}>
-                  <AiOutlineLogin />
-                  <Link to={'account/login'}>Sign in</Link>
-                </List>
-                <List toggle={dropdown.account ? true : false}>
-                  <FaSlack />
-                  <Link to={'account/slack'}>Slack</Link>
-                </List>
-              </Dropdown>
+          <div id="dropdowns">
+            {toggle ? (
+              <>
+                <Dropdown id="account" onClick={handleDropdown}>
+                  <Title>
+                    <MdAccountCircle />
+                    Account
+                  </Title>
+                  <List toggle={dropdown.account ? true : false}>
+                    <AiOutlineLogin />
+                    <Link to={'account/login'}>Sign in</Link>
+                  </List>
+                  <List toggle={dropdown.account ? true : false}>
+                    <FaSlack />
+                    <Link to={'account/slack'}>Slack</Link>
+                  </List>
+                </Dropdown>
+                <Dropdown id="bot" onClick={handleDropdown}>
+                  <Title>
+                    <AiOutlineRobot />
+                    Bot
+                  </Title>
+                  <List toggle={dropdown.bot ? true : false}>
+                    <AiOutlineMoneyCollect />
+                    <Link to={'bot/faucet-getter'}>Faucet</Link>
+                  </List>
+                  <List toggle={dropdown.bot ? true : false}>
+                    <AiOutlineLock />
+                    <Link to={'bot/env-notifier'}>Dotenv</Link>
+                  </List>
+                </Dropdown>
+                <Dropdown id="help" onClick={handleDropdown}>
+                  <Title>
+                    <MdOutlineContactSupport />
+                    About
+                  </Title>
+                  <List toggle={dropdown.help ? true : false}>
+                    <AiOutlineMail />
+                    <Link to={'help/contact'}>Contact</Link>
+                  </List>
+                </Dropdown>
 
-              <Dropdown id="bot" onClick={handleDropdown}>
-                <Title>
-                  <AiOutlineRobot />
-                  Bot
-                </Title>
-                <List toggle={dropdown.bot ? true : false}>
-                  <AiOutlineMoneyCollect />
-                  <Link to={'bot/faucet-getter'}>Faucet</Link>
-                </List>
-                <List toggle={dropdown.bot ? true : false}>
-                  <AiOutlineLock />
-                  <Link to={'bot/env-notifier'}>Dotenv</Link>
-                </List>
-              </Dropdown>
-
-              <Dropdown id="help" onClick={handleDropdown}>
-                <Title>
-                  <MdOutlineContactSupport />
-                  Help
-                </Title>
-                <List toggle={dropdown.help ? true : false}>
-                  <AiOutlineMail />
-                  <Link to={'help/contact'}>Contact</Link>
-                </List>
-              </Dropdown>
-            </div>
-          )}
+                <WrapperDivForCenter id="fragment">Made with love by @asunlabs</WrapperDivForCenter>
+              </>
+            ) : (
+              <></>
+            )}
+          </div>
         </Navbar>
-        <Body id="body">{children}</Body>
-
-        {/* render toast only once */}
-        <ToastContainer />
+        {/* prettier-ignore */}
+        <div onClick={handleOutsideDropdown}>
+          {children}
+        </div>
       </Background>
     </>
   );
