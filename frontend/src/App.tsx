68e64611e8bc59d5399a0b0d1d5d51bf0ed44c6d
developerasun <nellow1102@gmail.com>
2023-03-02 22:47:16 +0900

    ENH:add helment to enable payhip js

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,36 +1,37 @@
-import { SlackContextProvider, WailsResponseContextProvider } from './context/Context';
+import { WailsResponseContextProvider } from './context/Context';
 import { HashRouter, Routes, Route } from 'react-router-dom';
 import EnvNotifier from './pages/bot/EnvNotifier';
 import { Layout } from './pages/Layout';
-import { SlackConfig } from './pages/account/SlackConfig';
-import { FaucetGetter } from './pages/bot/FaucetGetter';
 import { Login } from './pages/account/Login';
 import { App_ } from './App_';
 import './App.css';
 import { Contact } from './pages/help/Contact';
 import { ToastContainer } from 'react-toastify';
 import { PageNotFound } from './pages/Page404';
+import { Helmet } from 'react-helmet';
 
 function App() {
   return (
     <div id="app">
       <HashRouter>
         <WailsResponseContextProvider>
-          <SlackContextProvider>
-            <Layout>
-              <Routes>
-                <Route path="/" element={<App_ />} />
-                <Route path="account/login" element={<Login />} />
-                <Route path="account/slack" element={<SlackConfig />} />
-                <Route path="bot/faucet-getter" element={<FaucetGetter />} />
-                <Route path="bot/env-notifier" element={<EnvNotifier />} />
-                <Route path="help/contact" element={<Contact />} />
-                <Route path="*" element={<PageNotFound />} />
-              </Routes>
-            </Layout>
-            {/* render toast only once */}
-            <ToastContainer />
-          </SlackContextProvider>
+          <Helmet>
+            <meta charSet="utf-8" />
+            <title>Owlly</title>
+            <link rel="canonical" href="http://mysite.com/example" />
+            <script type="text/javascript" src="https://payhip.com/payhip.js"></script>
+          </Helmet>
+          <Layout>
+            <Routes>
+              <Route path="/" element={<App_ />} />
+              <Route path="account/login" element={<Login />} />
+              <Route path="bot/env-notifier" element={<EnvNotifier />} />
+              <Route path="help/contact" element={<Contact />} />
+              <Route path="*" element={<PageNotFound />} />
+            </Routes>
+          </Layout>
+          {/* render toast only once */}
+          <ToastContainer />
         </WailsResponseContextProvider>
       </HashRouter>
     </div>
